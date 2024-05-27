05b0797118cd634a15acd2339acb79c49391217d
zainab_majid <zainabmajid2408@gmail.com>
2024-05-27 10:40:54 -0600

    creating the logic of the game

--- hangman.py
+++ hangman.py
@@ -5,3 +5,11 @@ from words import word_list #from words file
 def getWord():
     word = random.choice(word_list)
     return word.upper() #return the chosen work in uppercase to make comparison easier.
+
+#
+def play(word):
+    wordCompleted = "_" * len(word) #initialize the word with _ for evry unguessed letter
+    guessed = False
+    guessedLetters = [] #holds the letters the user guesses
+    guessedWords = [] #holds the words the user guessed to avoid repeats
+    tries = 6 #number of body parts left to be drawn on hangman before user loses
