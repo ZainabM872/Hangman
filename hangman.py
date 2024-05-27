9481db0ec95cc22e0eb95d275165ecba094ccde1
zainab_majid <zainabmajid2408@gmail.com>
2024-05-27 10:43:15 -0600

    done displayHangman()

--- hangman.py
+++ hangman.py
@@ -13,3 +13,79 @@ def play(word):
     guessedLetters = [] #holds the letters the user guesses
     guessedWords = [] #holds the words the user guessed to avoid repeats
     tries = 6 #number of body parts left to be drawn on hangman before user loses
+
+#displays the hangman in different stages as the user guesses
+#needed help from: https://github.com/kiteco/python-youtube-code/blob/master/build-hangman-in-python/hangman.py
+def displayHangman(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries] #depending on how many tries are left, it returns an index in the list
